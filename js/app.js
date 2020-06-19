3e24c8410771f7482185caf35a50361b531c20c4
tran duy linh <nhocdl.poro1@gmail.com>
2020-06-19 19:00:51 +0700

    apha 1.1

--- js/app.js
+++ js/app.js
@@ -1,62 +1,102 @@
-$(document).ready(function () {
-    var animal = [
-        "images/animals/animals(1).jpg",
-        "images/animals/animals(2).jpg",
-        "images/animals/animals(3).jpg",
-        "images/animals/animals(4).jpg",
-        "images/animals/animals(5).jpg",
-    ];
+const thing = [
+    {
+        name: "cat",
+        src: "images/animals/animals (1).jpg"
+    },
+    {
+        name: "nature",
+        src: "images/nature/nature (2).jpg"    
+    },
+    {
+        name: "dog",
+        src: "images/animals/animals (2).jpg"
+    },
+    {
+        name: "place",
+        src : "images/places/arch (1).jpg"
+    },
+    {
+        name: "place",
+        src: "images/places/arch (3).jpg"
+    },
+    {
+        name: "cat",
+        src: "images/animals/animals (4).jpg"
+    }, 
+    {
+        name: "nature",
+        src: "images/nature/nature (3).jpg"    
+    }, 
+    {
+        name: "dog",
+        src: "images/animals/animals (3).jpg"
+    }, 
+    {
+        name: "nature",
+        src: "images/nature/nature (4).jpg"    
+    },
+    {
+        name: "place",
+        src: "images/places/arch (2).jpg"
+    }, 
+    {
+        name: "dog",
+        src: "images/animals/animals (5).jpg"
+    }, 
+    {
+        name: "nature",
+        src: "images/nature/nature (1).jpg"
+    }, 
+    {
+        name: "place",
+        src: "images/places/arch (4).jpg"
+    },
+    {
+        name: "place",
+        src : "images/places/arch (5).jpg"
+    }, 
+    {
+        name: "nature",
+        src: "images/nature/nature (5).jpg"    
+    },
 
-    var nature = [
-        "images/nature/nature(1).jpg",
-        "images/nature/nature(2).jpg",
-        "images/nature/nature(3).jpg",
-        "images/nature/nature(4).jpg",
-        "images/nature/nature(5).jpg"
-    ]
+]
 
-
-
-    for (i = 1; i < (animal.length + 1); i++) {
+console.log(thing[0])
+    for (i = 0; i < (thing.length ); i++) {
 $("#phan-anh-duoc-loc").append(`<div class="card position-relative hinh-loc" style="width: 18rem;">` +
- `<img class="card-img-top" data-image-type="nature" src="images/nature/nature (${[i]}).jpg" alt="Card image cap"> ` +
-  ` <p class="title-hinh position-absolute">nature</p> `+
+ `<img class="card-img-top" data-image-type="${thing[i]["name"]}" src="${[thing[i]["src"]]}" alt="Card image cap"> ` +
+  ` <p class="title-hinh position-absolute">${thing[i]["name"]}</p> `+
 `</div>`
 )
 }
 
-        for (i = 1; i < (nature.length + 1); i++) {
-$("#phan-anh-duoc-loc").append(`<div class="card position-relative hinh-loc" style="width: 18rem;">` +
- `<img class="card-img-top" data-image-type="animal" src="images/animals/animals (${[i]}).jpg" alt="Card image cap"> ` +
-  ` <p class="title-hinh position-absolute">animal</p> `+
-`</div>`
-)
-}
-
-    for (i = 1; i < (animal.length + 1); i++) {
-$("#phan-anh-duoc-loc").append(`<div class="card position-relative hinh-loc" style="width: 18rem;">` +
- `<img class="card-img-top"  data-image-type="place" src="images/places/arch (${[i]}).jpg" alt="Card image cap"> ` +
-  ` <p class="title-hinh position-absolute">place</p> `+
-`</div>`
-)
-}
-
+    
 
 const imageSelect = $("img")
 
 function chay() {
     $(".nut-bam").on('click', function() {
-        if(this.getAttribute("data-nut") === "animal") {
-            imageSelect.parent().fadeOut(400);
-            $('img[data-image-type="animal"]').parent().fadeIn(900) 
+        if(this.getAttribute("data-nut") === "dog") {
+            imageSelect.parent().hide(100)
+            $('img[data-image-type="dog"]').parent().fadeIn(400) 
         } 
         if(this.getAttribute("data-nut") === "nature") {
-            imageSelect.parent().fadeOut(400);
-            $('img[data-image-type="nature"]').parent().fadeIn(900) 
+            imageSelect.parent().hide(100);
+            $('img[data-image-type="nature"]').parent().fadeIn(400) 
         }
         if (this.getAttribute("data-nut") === "city"){
-            imageSelect.parent().fadeOut(400);
-            $('img[data-image-type="place"]').parent().fadeIn(900) 
+            imageSelect.parent().hide(100);
+            $('img[data-image-type="place"]').stop().parent().fadeIn(400) 
+        } 
+        if (this.getAttribute("data-nut") === "all"){
+            imageSelect.parent().hide(100);
+            $('img').stop().parent().fadeIn(400) 
+        } 
+        if (this.getAttribute("data-nut") === "animal"){
+            imageSelect.parent().hide(100);
+            $('img[data-image-type="cat"]').stop().parent().fadeIn(400) 
+            $('img[data-image-type="dog"]').stop().parent().fadeIn(400) 
         }
         })
 }
@@ -64,5 +104,3 @@ function chay() {
 
 chay()
 
-
-});
